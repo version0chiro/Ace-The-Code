26cd10c358f54d6dc4e5eb7776eeb386683ba17f
Chiro <sachinsinghroxx@gmail.com>
2021-06-07 17:14:37 +0530

    added stack using queue with explaination

---
+++ stacks&queues/StackUsing2queue/main.cpp
@@ -0,0 +1,79 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class QueueStack{
+private:
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int);
+    int pop();
+};
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        QueueStack *qs = new QueueStack();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            qs->push(a);
+        }else if(QueryType==2){
+            cout<<qs->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+/* The structure of the class is
+class QueueStack{
+private:
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int);
+    int pop();
+};
+ */
+
+//Function to push an element into stack using two queues.
+void QueueStack :: push(int x)
+{
+        // Your Code
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+}
+
+//Function to pop an element from stack using two queues. 
+int QueueStack :: pop()
+{
+     //code here
+     if(q1.size()<1) return -1;
+    int temp = q1.front();
+    q1.pop();
+    return temp;
+        // Your Code       
+}
