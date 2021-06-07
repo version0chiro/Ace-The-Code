3a9257daacf7739af9b45c00ee5729738549914d
Chiro <sachinsinghroxx@gmail.com>
2021-06-08 00:10:15 +0530

    rivision cyclic rotation

--- array/cyclicRotation/main.cpp
+++ array/cyclicRotation/main.cpp
@@ -1,19 +1,38 @@
-#include <bits/stdc++.h>
+// { Driver Code Starts
+#include <stdio.h>
 
-using namespace std;
+void rotate(int arr[], int n);
 
-int main(){
-    int arr[] = {1,2,3,4,5};
-    int n = 5;//sizeof(arr)/sizeof(arr[0]);
-    int temp = arr[n-1];
-    for(int i=n-1; i>=0; i--){
-        arr[i] = arr[i-1];
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
     }
-    arr[0] = temp;
-    cout<<endl;
-    for(auto a:arr){
-        cout<<a<<" "<<endl;
-    }
+        return 0;
+}
+// } Driver Code Ends
 
-    return 0;
+
+void rotate(int arr[], int n)
+{
+    int last=arr[n-1];
+    int temp=arr[0];
+    for(int i=1;i<n;i++){
+        int t2=arr[i];
+        arr[i]=temp;
+        temp=t2;
+    }
+    
+    arr[0]=last;
 }
