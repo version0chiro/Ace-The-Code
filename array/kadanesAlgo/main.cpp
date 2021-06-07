a4cc183f9941c8c35db7f10279f264e49e415e45
Chiro <sachinsinghroxx@gmail.com>
2021-06-08 00:17:57 +0530

    rivison kadane

--- array/kadanesAlgo/main.cpp
+++ array/kadanesAlgo/main.cpp
@@ -1,25 +1,55 @@
-#include <bits/stdc++.h>
-
+// { Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(){
 
-    int n=5;
-
-    int arr[] = {1,2,3,-2,5};
-
-    int max_so_far = INT_MIN,max_ending_here=0;
-
-    for (int i=0;i<n;i++){
-        max_ending_here = max_ending_here + arr[i];
-        if(max_ending_here<0)
-            max_ending_here = 0;
-        if(max_so_far<max_ending_here){
-            max_so_far = max_ending_here;
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int maxMain =INT_MIN;
+        
+        int sumSoFar=0;
+        
+        
+        for(int i=0;i<n;i++){
+            sumSoFar+=arr[i];
+            if(sumSoFar<0){
+                sumSoFar=0;
+            }
+            
+            maxMain=max(maxMain,sumSoFar);
         }
+        
+        return maxMain;
     }
+};
 
-    cout<<max_so_far<<endl;
+// { Driver Code Starts.
 
-    return 0;
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
 }
+  // } Driver Code Ends
