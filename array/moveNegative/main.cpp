fd02ae402b7e164dff451da66403c8fb195137a2
Chiro <sachinsinghroxx@gmail.com>
2021-06-07 23:55:49 +0530

    revision of array: move negative

--- array/moveNegative/main.cpp
+++ array/moveNegative/main.cpp
@@ -1,36 +1,56 @@
+// { Driver Code Starts
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(){
-
-    int arr[] = {-12,11,-13,-5,6,-7,5,-3,-6};
-    stack<int> negativeNumbers;
-    stack<int> positiveNumbers;
-
-    for(auto a:arr){
-        if(a<0){
-            negativeNumbers.push(a);
+ // } Driver Code Ends
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        // Your code goes here
+        stack<int> neg;
+        stack<int> pos;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]<0){
+                neg.push(arr[i]);
+            }else{
+                pos.push(arr[i]);
+            }
         }
-        else{
-            positiveNumbers.push(a);
+        
+        int j=n-1;
+        while(!neg.empty()){
+            arr[j]=neg.top();
+            j--;
+            neg.pop();
+        }
+        while(!pos.empty()){
+            arr[j]=pos.top();
+            j--;
+            pos.pop();
         }
     }
-    int i=0;
-    while(negativeNumbers.size()>0){
-        arr[i++]=negativeNumbers.top();
-        negativeNumbers.pop();
-    }
-    
-    while(positiveNumbers.size()>0){
-        arr[i++]=positiveNumbers.top();
-        positiveNumbers.pop();
-    }
+};
 
-    for(auto a:arr){
-        cout<<a<<endl;
-    }
-
-
-    return 0;
+// { Driver Code Starts.
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		ob.segregateElements(a,n);
+		
+        for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+		cout<<endl;
+	}
 }
+  // } Driver Code Ends
