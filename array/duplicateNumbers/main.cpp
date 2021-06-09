9e2221b693e765468dc736968a7d830402da8ea2
Chiro <sachinsinghroxx@gmail.com>
2021-06-09 16:34:32 +0530

    added constant space to find duplicate number

--- array/duplicateNumbers/main.cpp
+++ array/duplicateNumbers/main.cpp
@@ -2,32 +2,57 @@
 
 using namespace std;
 
-int findDuplicate(vector<int>& nums){
+int findDuplicate(vector<int> &nums)
+{
     set<int> seenNumbers;
-        for(auto a:nums){
-            // cout<<a;
-            if (seenNumbers.find(a) != seenNumbers.end()) { 
-            return a; 
+    for (auto a : nums)
+    {
+        // cout<<a;
+        if (seenNumbers.find(a) != seenNumbers.end())
+        {
+            return a;
         }
-            else{
-                seenNumbers.insert(a);
-            }
+        else
+        {
+            seenNumbers.insert(a);
         }
-        return -1;
+    }
+    return -1;
 }
 
-int main(){
+int findDuplicateConstantSpace(vector<int> &nums)
+{
+    int slow = nums[0];
+    int fast = nums[nums[0]];
+    while (slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+
+    fast = 0;
+    while (fast != slow)
+    {
+        fast = nums[fast];
+        slow = nums[slow];
+    }
+
+    return slow;
+}
+
+int main()
+{
 
     int n;
     int temp;
-    cin>>n;
+    cin >> n;
     vector<int> array;
-    for(int i=0;i<n;i++){
-        cin>>temp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
         array.push_back(temp);
     }
 
-    cout<< "duplicate number=>"<<findDuplicate(array)<<endl;
-    return 0; 
-
+    cout << "duplicate number=>" << findDuplicate(array) << endl;
+    return 0;
 }
