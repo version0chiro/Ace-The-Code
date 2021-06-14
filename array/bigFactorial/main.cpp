1cd310ce2e12307374f9ad0bfe5d63692234e361
Chiro <sachinsinghroxx@gmail.com>
2021-06-14 14:31:43 +0530

    added new big fact code

--- array/bigFactorial/main.cpp
+++ array/bigFactorial/main.cpp
@@ -1,57 +1,72 @@
-#include <iostream>
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
 using namespace std;
 
-#define MAX 50000
+// } Driver Code Ends
+//User function template for C++
 
-int multiply(int x,int arr[],int arr_size);
+class Solution
+{
+public:
+    int multiply(int x, vector<int> &res, int res_size)
+    {
+        int c = 0;
+        for (int i = 0; i < res_size; i++)
+        {
+            int p = res[i] * x + c;
+            res[i] = p % 10;
+            c = p / 10;
+        }
 
+        while (c)
+        {
+            res[res_size] = c % 10;
+            c = c / 10;
+            res_size++;
+        }
 
-void factorial(int n){
-    int res[MAX];
-    
-    int res_size=1;
-    
-    res[0]=1;
-    
-    for(int x=2;x<=n;x++){
-        res_size=multiply(x,res,res_size);
+        return res_size;
     }
-    
-    for(int n=res_size-1;n>-1;n--){
-        cout<<res[n];
+    vector<int> factorial(int N)
+    {
+        // code here
+        vector<int> res(50000);
+        res[0] = 1;
+        int resSize = 1;
+        for (int x = 2; x <= N; x++)
+        {
+            resSize = multiply(x, res, resSize);
+        }
+
+        vector<int> ans;
+
+        for (int i = resSize - 1; i >= 0; i--)
+        {
+            ans.push_back(res[i]);
+        }
+
+        return ans;
     }
-}
+};
 
-int multiply(int x,int arr[],int arr_size){
-    int carry =0;
-    for(int i=0;i<arr_size;i++){
-        int product = arr[i]*x + carry;
-        arr[i] = product % 10;
-        carry = product/10;
-        
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        vector<int> result = ob.factorial(N);
+        for (int i = 0; i < result.size(); ++i)
+        {
+            cout << result[i];
+        }
+        cout << endl;
     }
-    
-    while(carry){
-        arr[arr_size] = carry%10;
-        carry = carry/10;
-        arr_size++;
-    }
-    
-    return arr_size;
-    
-}
-
-
-int main() {
-	//code
-	int t;
-	cin>>t;
-	while(t--){
-	    int input;
-	    cin>>input;
-        factorial(input);
-	    cout<<endl;
-	}
-
-	return 0;
-}
+    return 0;
+} // } Driver Code Ends
