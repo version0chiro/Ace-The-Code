7b4dab0fed543dd6c26fa867d9488f7ee578ad42
Chiro <sachinsinghroxx@gmail.com>
2021-06-14 14:41:57 +0530

    updated max sub array solution

--- array/maximumProductSubarray/main.cpp
+++ array/maximumProductSubarray/main.cpp
@@ -3,75 +3,45 @@
 
 using namespace std;
 
- // } Driver Code Ends
-
-
+// } Driver Code Ends
 //User function template for C++
-class Solution{
+class Solution
+{
 public:
-
 	// Function to find maximum product subarray
-	long long maxProduct(int *arr, int n) {
-	    // code here
-	    long long  minSoFar=1;
-	    long long  maxSoFar=1;
-	    
-	    long long one=1;
-	    
-	    long long finalProd=0;
-	    bool flag=0;
-	    
-	    
-	    for(int i=0;i<n;i++){
-	        if(arr[i]>0){
-	            maxSoFar = maxSoFar*arr[i];
-	            minSoFar = min(minSoFar*arr[i],one);
-	            flag=1;
-	            
-	        }
-	        else if (arr[i]==0){
-	            maxSoFar=one;
-	            minSoFar=one;
-	        }
-	        else{
-	            long long temp = maxSoFar;
-	            maxSoFar=max(minSoFar*arr[i],one);
-	            minSoFar = temp*arr[i];
-	        }
-	        
-	        if(maxSoFar>finalProd){
-	            finalProd = maxSoFar;
-	        }
-	        
-	        
-	    }
-	    
-	    if(flag==0 && finalProd ==0){
-	        return 0;
-	    }
-	    
-	    return finalProd;
-	    
-	    
-	    
+	long long maxProduct(int *arr, int n)
+	{
+		// code here
+		long long l = 1, r = 1;
+		long long maxV = INT_MIN;
+		for (int i = 0; i < n; i++)
+		{
+			l == 0 ? l = arr[i] : l = l * arr[i];
+			r == 0 ? r = arr[n - i - 1] : r = r * arr[n - i - 1];
+			maxV = max(maxV, max(l, r));
+		}
+		return maxV;
 	}
 };
 
 // { Driver Code Starts.
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, i;
-        cin >> n;
-        int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        Solution ob;
-        auto ans = ob.maxProduct(arr, n);
-        cout << ans << "\n";
-    }
-    return 0;
-}  // } Driver Code Ends
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, i;
+		cin >> n;
+		int arr[n];
+		for (i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+		Solution ob;
+		auto ans = ob.maxProduct(arr, n);
+		cout << ans << "\n";
+	}
+	return 0;
+} // } Driver Code Ends
