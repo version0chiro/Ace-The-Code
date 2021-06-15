635beb5de5278812a3f29ac14bdf10af4f811a0e
Chiro <sachinsinghroxx@gmail.com>
2021-06-15 15:56:32 +0530

    added three way partition

--- array/threeWayPartition/main.cpp
+++ array/threeWayPartition/main.cpp
@@ -1,128 +1,61 @@
 // { Driver Code Starts
-//Initial template for C++
 
 #include <bits/stdc++.h>
-#include <unordered_map>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 
-
-//User function template for C++
-
-class Solution{   
+class Solution
+{
 public:
-    void threeWayPartition(vector<int>& array,int a, int b)
+    int sb(int arr[], int n, int x)
     {
-        // code here 
-        multiset<int> before;
-        multiset<int> middle;
-        multiset<int> after;
-        vector<int> answer;
-        for(auto p:array){
-            if(p<a){
-                before.insert(p);
+        // Your code goes here
+        int len = INT_MAX;
+        int tempLen = 0;
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        while (i < n && j < n)
+        {
+            // cout<<" "<<arr[i]<<" ";
+            if (sum <= x)
+            {
+                sum += arr[j];
+                tempLen++;
+                // cout<<" adding "<<arr[j]<<" "<<sum<<" ";
+                j++;
             }
-            else if (p>b){
-                after.insert(p);
+            while (sum > x)
+            {
+                len = min(len, tempLen);
+                tempLen--;
+                sum = sum - arr[i];
+                // cout<<" removing "<<arr[i]<<" "<<sum<<" "<<" "<<tempLen;
+                i++;
             }
-            else if (p>=a && p<=b){
-                middle.insert(p);
-            }
         }
-        
-        for(auto t:before){
-            answer.push_back(t);
-        }
-        for(auto t:middle){
-            answer.push_back(t);
-        }
-        for(auto t:after){
-            answer.push_back(t);
-        }
-        
-        array=answer;
-        // for(auto a:answer){
-        //     cout<<a<<" ";
-        // }
+
+        return len;
     }
 };
 
 // { Driver Code Starts.
 
-int main() {
+int main()
+{
+    // your code goes here
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int N;
-        cin>>N;
-        
-        vector<int> array(N);
-        unordered_map<int,int> ump;
-        
-        for(int i=0;i<N;i++){
-            cin>>array[i];
-            ump[array[i]]++;
-        }
-        
-        int a,b;
-        cin>>a>>b;
-        
-        vector<int> original = array;
-
-        int k1=0,k2=0,k3=0;
-        int kk1=0;int kk2=0;int kk3=0;
-        
-        for(int i=0; i<N; i++)
-        {
-            if(original[i]>b)
-                k3++;
-            else if(original[i]<=b and original[i]>=a)
-                k2++;
-            else if(original[i]<b)
-                k1++;
-        }
-        
-        Solution ob;
-        ob.threeWayPartition(array,a,b);
-      
-        for(int i=0;i<k1;i++)
-        {
-            if(array[i]<b)
-            kk1++;
-        }
-        
-        for(int i=k1;i<k1+k2;i++)
-        {
-            if(array[i]<=b and array[i]>=a)
-            kk2++;
-            
-        }
-        
-        for(int i=k1+k2;i<k1+k2+k3;i++)
-        {
-            if(array[i]>b)
-            kk3++;
-        }
-        bool ok = 0;
-        if(k1==kk1 and k2 ==kk2 and k3 == kk3)
-            ok = 1;
-        
-        for(int i=0;i<array.size();i++)
-            ump[array[i]]--;
-        
-        for(int i=0;i<array.size();i++)
-            if(ump[array[i]]!=0)
-                ok=0;
-        
-        if(ok)
-            cout<<1<<endl;
-        else
-            cout<<0<<endl;
-        
+        int n, x;
+        cin >> n >> x;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution obj;
+        cout << obj.sb(a, n, x) << endl;
     }
-    
     return 0;
-}
-  // } Driver Code Ends
+} // } Driver Code Ends
