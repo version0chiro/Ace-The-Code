b9c4abbe5e0306b347209efc9fd45fb78da8d381
Chiro <sachinsinghroxx@gmail.com>
2021-07-09 22:08:57 +0530

    added y insertion in LL

---
+++ linkedList/findYinsertion/main.cpp
@@ -0,0 +1,109 @@
+// { Driver Code Starts
+#include<iostream>
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node* head1, struct Node* head2);
+
+Node* inputList(int size)
+{
+    if(size==0) return NULL;
+    
+    int val;
+    cin>> val;
+    
+    Node *head = new Node(val);
+    Node *tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n1,n2,n3;
+
+    cin>>T;
+    while(T--)
+    {
+        cin>>n1>>n2>>n3;
+        
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+        
+        Node* temp = head1;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        temp = head2;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        cout << intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node* curr = head1;
+    while(curr){
+        curr->data=curr->data+2000;
+        curr=curr->next;
+    }
+    
+    curr=head2;
+    while(curr){
+        // cout<<curr->data<<" ";
+        if(curr->data>1000){
+            if(curr->data>=2000)
+                return curr->data-2000;
+            else
+                return (2000-curr->data)*-1;
+        }
+        // curr->data=-1;
+        curr=curr->next;
+    }
+    
+    return -1;
+    
+}
+
